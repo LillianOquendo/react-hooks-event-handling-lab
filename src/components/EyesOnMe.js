0f3bba793020cc041685b233c2b082a24337c30e
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-05-26 13:21:02 -0400

    React Hooks and Handling Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
+import React from "react";
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    return (
+        <button 
+        onFocus={ () => {console.log("Good!")}}
+        onBlur={ () => {console.log("Hey! Eyes on me!")}}
+        >Eyes on me
+        </button>    
+    )
+}
+
+export default EyesOnMe;
